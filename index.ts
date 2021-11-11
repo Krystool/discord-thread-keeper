7352966f4516da2af6700f942f2638b9fbf1fda5
Simon Brugel <sbrugel62@gmail.com>
2021-11-11 17:45:56 -0500

    feat: template for unarchiving threads

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import DiscordJS, { Intents } from 'discord.js'
+import DiscordJS, { Intents, TextChannel, ThreadChannel, GuildChannel } from 'discord.js'
 import WOKCommands from 'wokcommands'
 import path from 'path'
+
 import { BOT } from './config'
 
 const client = new DiscordJS.Client({
@@ -18,6 +19,18 @@ client.on('ready', () => {
         typeScript: true,
         testServers: ['679777315814637683'],
     })
+
+    main()
 })
+
+async function main() {
+    let parent = (await client.channels.fetch('904838634950258770')) as GuildChannel;
+    if (parent.isText()) {
+        let thread = await parent.threads.fetch('907971826926362655') as ThreadChannel
+        thread.setArchived(false);
+    } else {
+        throw `Something went wrong!'`;
+    }
+}
 
 client.login(BOT.TOKEN)
