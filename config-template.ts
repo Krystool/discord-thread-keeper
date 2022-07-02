3168e1dfdec5cf77151c985e1d2f31cbfdc4751d
Krystool <neopu.contact@gmail.com>
2022-07-02 19:25:25 +0200

    changing config to actual one

--- config-template.ts
+++
@@ -1,5 +0,0 @@
-export const BOT = {
-	TOKEN: 'Your bot token here.',
-    SERVER_CHANNEL: 'The channel ID of ANY channel on the server you intend to use this bot on.',
-    THREADS: ['Thread IDs here', 'Separate threads', 'With commas']
-};
---
+++ config.ts
@@ -0,0 +1,5 @@
+export const BOT = {
+	TOKEN: 'process.env.TOKEN',
+    SERVER_CHANNEL: ['992843251889492018','701059616221560902'],
+    THREADS: ['Thread IDs here', 'Separate threads', 'With commas']
+};
